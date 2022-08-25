a5df53e5ace7e7d683c1ac6e9197c8d3906667c7
melxin <melxin@tutamail.com>
2022-08-25 09:10:00 +0200

    deob: fix deobfuscator, fernflower
    Usage tasks:
    1. deobfuscator -> tasks -> gamepack -> Downloader$main() -> Deob$main() -> UpdateMappings$main()
    
    2. OpenOSRS -> other -> decompile
    
    Decompiled files are in projectDir/injected-client/build/decompiled-sources/updated-${project.version}.jar

--- buildSrc/src/main/kotlin/FernflowerTask.kt
+++ buildSrc/src/main/kotlin/FernflowerTask.kt
@@ -18,7 +18,7 @@ open class FernflowerTask: DefaultTask() {
 
     @Input
     @Optional
-    var inputJar: String? = null
+    var inputJar: String? = "/home/t7emon/Documents/runescape/runelite/deobfuscator/build/libs/updated-${project.version}.jar"
 
     @Input
     @Optional
