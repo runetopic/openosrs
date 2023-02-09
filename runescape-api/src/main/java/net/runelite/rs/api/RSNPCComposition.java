7df2c1e07624447f36f471d873e7245d186f6e49
melxin <melxin@tutamail.com>
2023-02-09 17:28:49 +0100

    project: rev 211

--- runescape-api/src/main/java/net/runelite/rs/api/RSNPCComposition.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSNPCComposition.java
@@ -64,9 +64,6 @@ public interface RSNPCComposition extends NPCComposition
 	@Import("headIconSpriteIndex")
 	short[] getHeadIconSpriteIndexes();
 
-	@Import("defaultHeadIconArchive")
-	int getDefaultHeadIconArchive();
-
 	@Import("params")
 	RSIterableNodeHashTable getParams();
 
