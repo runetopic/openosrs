f5803af249962d31f44d834578fa3886b1a722a0
melxin <melxin@tutamail.com>
2023-02-28 21:43:08 +0100

    Revert "ConfigManager: only key rsprofiles off of account hash"
    This reverts commit 4a8e6a39

--- runelite-client/src/main/java/net/runelite/client/config/RuneScapeProfile.java
+++ runelite-client/src/main/java/net/runelite/client/config/RuneScapeProfile.java
@@ -37,6 +37,7 @@ public class RuneScapeProfile
 
 	private final String displayName;
 	private final RuneScapeProfileType type;
+	private final byte[] loginHash;
 	private final long accountHash;
 
 	/**
