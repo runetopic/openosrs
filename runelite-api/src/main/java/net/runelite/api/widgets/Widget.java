ec668033e60a07fa104f57f5254e4b5e20fe7df3
melxin <melxin@tutamail.com>
2023-03-29 00:56:39 +0200

    mixins: fix widget item

--- runelite-api/src/main/java/net/runelite/api/widgets/Widget.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/Widget.java
@@ -1131,14 +1131,4 @@ public interface Widget
 	 * Gets the image which is (or should be) drawn on this widget
 	 */
 	SpritePixels getSprite();
-
-	/**
-	 * Sets the X padding between widgets, mainly for inventory items
-	 */
-	void setPaddingX(int val);
-
-	/**
-	 * Sets the Y padding between widgets, mainly for inventory items
-	 */
-	void setPaddingY(int val);
 }
