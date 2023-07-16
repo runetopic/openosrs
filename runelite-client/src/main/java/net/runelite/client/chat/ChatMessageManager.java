084127fe80d0c62d4b31069be94baf9882202110
melxin <melxin@tutamail.com>
2023-07-16 16:20:48 +0200

    Revert "checkstyle: no trailing whitespace on empty lines"
    This reverts commit 5c0d87c9

--- runelite-client/src/main/java/net/runelite/client/chat/ChatMessageManager.java
+++ runelite-client/src/main/java/net/runelite/client/chat/ChatMessageManager.java
@@ -226,7 +226,7 @@ public class ChatMessageManager
 
 		final String[] stringStack = client.getStringStack();
 		final int stringStackSize = client.getStringStackSize();
-
+		
 		String fromToUsername = stringStack[stringStackSize - 1];
 		if (wrap)
 		{
@@ -821,7 +821,7 @@ public class ChatMessageManager
 
 		// Update the message with RuneLite additions
 		line.setRuneLiteFormatMessage(message.getRuneLiteFormattedMessage());
-
+		
 		if (message.getTimestamp() != 0)
 		{
 			line.setTimestamp(message.getTimestamp());
