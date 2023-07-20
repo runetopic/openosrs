jbx5/devious-client

rev 215.2: attempt to fix music
This is probably incorrect might want to re-do

runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -2605,13 +2605,20 @@ public abstract class RSClientMixin implements RSClient
 	@Inject
 	public void playMusicTrack(int var0, RSAbstractArchive var1, int var2, int var3, int var4, boolean var5)
 	{
-		client.setMusicPlayerStatus(1);
-		client.setMusicTrackArchive(var1);
-		client.setMusicTrackGroupId(var2);
-		client.setMusicTrackFileId(var3);
-		client.setMusicTrackVolume(var4);
-		client.setMusicTrackBoolean(var5);
-		client.setPcmSampleLength(var0);
+		for (RSMusicSong musicSong : client.getMusicSongs())
+		{
+			if (musicSong.getMusicTrackFileId() == var3)
+			{
+				client.setMusicPlayerStatus(1);
+				musicSong.setMusicTrackArchive(var1);
+				musicSong.setMusicTrackGroupId(var2);
+				musicSong.setMusicTrackFileId(var3);
+				musicSong.setMusicTrackVolume(var4);
+				musicSong.setMusicTrackBoolean(var5);
+				//musicSong.setPcmSampleLength(var0);
+				break;
+			}
+		}
 	}
 
 	@Inject
@@ -2713,16 +2720,17 @@ public abstract class RSClientMixin implements RSClient
 	@Override
 	public void setMusicVolume(int volume)
 	{
-		if (volume > 0 && client.getPreferences().getMusicVolume() <= 0 && client.getCurrentTrackGroupId() != -1)
+		RSMusicSong song = client.getMusicSongs().get(0);
+		if (volume > 0 && client.getPreferences().getMusicVolume() <= 0 && song.getMusicCurrentTrackId() != -1)
 		{
-			client.playMusicTrack(1000, client.getMusicTracks(), client.getCurrentTrackGroupId(), 0, volume, false);
+			client.playMusicTrack(1000, client.getMusicTracks(), song.getMusicCurrentTrackId(), 0, volume, false);
 		}
 
 		client.getPreferences().setMusicVolume(volume);
-		client.setMusicTrackVolume(volume);
-		if (client.getMidiPcmStream() != null)
+		song.setMusicTrackVolume(volume);
+		if (song.getMidiPcmStream() != null)
 		{
-			client.getMidiPcmStream().setPcmStreamVolume(volume);
+			song.getMidiPcmStream().setPcmStreamVolume(volume);
 		}
 	}
 

runescape-client/src/main/java/ObjectComposition.java
@@ -970,23 +970,23 @@ public class ObjectComposition extends DualNode {
 				RouteStrategy.method4227(0, 0);
 			} else if (var5 != -1) {
 				boolean var6;
-				if (class306.field3398.isEmpty()) {
+				if (class306.musicSongs.isEmpty()) {
 					var6 = false;
 				} else {
-					class318 var7 = (class318)class306.field3398.get(0);
-					var6 = var7 != null && var5 == var7.field3507;
+					MusicSong var7 = (MusicSong)class306.musicSongs.get(0);
+					var6 = var7 != null && var5 == var7.musicTrackGroupId;
 				}
 
 				if (!var6 && class150.clientPreferences.method2478() != 0) {
 					ArrayList var9 = new ArrayList();
 
 					for (int var8 = 0; var8 < var0.size(); ++var8) {
-						var9.add(new class318(class13.archive6, (Integer)var0.get(var8), 0, class150.clientPreferences.method2478(), false));
+						var9.add(new MusicSong(class13.archive6, (Integer)var0.get(var8), 0, class150.clientPreferences.method2478(), false));
 					}
 
 					if (Client.playingJingle) {
-						class306.field3398.clear();
-						class306.field3398.addAll(var9);
+						class306.musicSongs.clear();
+						class306.musicSongs.addAll(var9);
 						ArchiveLoader.method2259(var1, var2, var3, var4);
 					} else {
 						UserComparator4.method2858(var9, var1, var2, var3, var4, false);

runescape-client/src/main/java/class405.java
@@ -8,7 +8,7 @@ public class class405 extends class407 {
 	@ObfuscatedSignature(
 		descriptor = "Lmj;"
 	)
-	class318 field4520;
+	MusicSong field4520;
 	@ObfuscatedName("ay")
 	@ObfuscatedGetter(
 		intValue = 2025464221
@@ -25,9 +25,9 @@ public class class405 extends class407 {
 		super.field4528 = "FadeOutTask";
 		if (var2 >= 0) {
 			if (var3 && var2 < class306.field3396.size()) {
-				this.field4520 = (class318)class306.field3396.get(var2);
+				this.field4520 = (MusicSong)class306.field3396.get(var2);
 			} else if (!var3 && var2 < class306.field3394.size()) {
-				this.field4520 = (class318)class306.field3394.get(var2);
+				this.field4520 = (MusicSong)class306.field3394.get(var2);
 			}
 
 			this.field4521 = var4;
@@ -40,19 +40,19 @@ public class class405 extends class407 {
 		garbageValue = "994462530"
 	)
 	public boolean vmethod7676(int var1) {
-		if (this.field4520 != null && this.field4520.field3516 != null) {
+		if (this.field4520 != null && this.field4520.midiPcmStream != null) {
 			this.field4520.field3515 = true;
 
 			try {
-				if (this.field4520.field3511 > 0.0F && this.field4520.field3516.isReady()) {
-					float var2 = this.field4521 == 0 ? (float)this.field4521 : (float)this.field4520.field3510 / (float)this.field4521;
-					class318 var10000 = this.field4520;
-					var10000.field3511 -= 0.0F == var2 ? (float)this.field4520.field3510 : var2;
+				if (this.field4520.field3511 > 0.0F && this.field4520.midiPcmStream.isReady()) {
+					float var2 = this.field4521 == 0 ? (float)this.field4521 : (float)this.field4520.musicTrackVolume / (float)this.field4521;
+					MusicSong var10000 = this.field4520;
+					var10000.field3511 -= 0.0F == var2 ? (float)this.field4520.musicTrackVolume : var2;
 					if (this.field4520.field3511 < 0.0F) {
 						this.field4520.field3511 = 0.0F;
 					}
 
-					this.field4520.field3516.setPcmStreamVolume((int)this.field4520.field3511);
+					this.field4520.midiPcmStream.setPcmStreamVolume((int)this.field4520.field3511);
 					return false;
 				}
 			} catch (Exception var4) {

runescape-client/src/main/java/class406.java
@@ -13,7 +13,7 @@ public class class406 extends class407 {
 	@ObfuscatedSignature(
 		descriptor = "Lmj;"
 	)
-	class318 field4524;
+	MusicSong field4524;
 	@ObfuscatedName("ay")
 	@ObfuscatedGetter(
 		intValue = 870968583
@@ -30,9 +30,9 @@ public class class406 extends class407 {
 		super.field4528 = "FadeInTask";
 		if (var2 >= 0) {
 			if (var3 && var2 < class306.field3396.size()) {
-				this.field4524 = (class318)class306.field3396.get(var2);
+				this.field4524 = (MusicSong)class306.field3396.get(var2);
 			} else if (!var3 && var2 < class306.field3394.size()) {
-				this.field4524 = (class318)class306.field3394.get(var2);
+				this.field4524 = (MusicSong)class306.field3394.get(var2);
 			}
 
 			this.field4523 = var4;
@@ -45,19 +45,19 @@ public class class406 extends class407 {
 		garbageValue = "994462530"
 	)
 	public boolean vmethod7676(int var1) {
-		if (this.field4524 != null && this.field4524.field3516 != null) {
+		if (this.field4524 != null && this.field4524.midiPcmStream != null) {
 			this.field4524.field3515 = true;
 
 			try {
-				if (this.field4524.field3511 < (float)this.field4524.field3510 && this.field4524.field3516.isReady()) {
-					float var2 = this.field4523 == 0 ? (float)this.field4523 : (float)this.field4524.field3510 / (float)this.field4523;
-					class318 var10000 = this.field4524;
-					var10000.field3511 += var2 == 0.0F ? (float)this.field4524.field3510 : var2;
-					if (this.field4524.field3511 > (float)this.field4524.field3510) {
-						this.field4524.field3511 = (float)this.field4524.field3510;
+				if (this.field4524.field3511 < (float)this.field4524.musicTrackVolume && this.field4524.midiPcmStream.isReady()) {
+					float var2 = this.field4523 == 0 ? (float)this.field4523 : (float)this.field4524.musicTrackVolume / (float)this.field4523;
+					MusicSong var10000 = this.field4524;
+					var10000.field3511 += var2 == 0.0F ? (float)this.field4524.musicTrackVolume : var2;
+					if (this.field4524.field3511 > (float)this.field4524.musicTrackVolume) {
+						this.field4524.field3511 = (float)this.field4524.musicTrackVolume;
 					}
 
-					this.field4524.field3516.setPcmStreamVolume((int)this.field4524.field3511);
+					this.field4524.midiPcmStream.setPcmStreamVolume((int)this.field4524.field3511);
 					return false;
 				}
 			} catch (Exception var4) {

runetopic/openosrs

runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -2654,13 +2654,20 @@ public abstract class RSClientMixin implements RSClient
 	@Inject
 	public void playMusicTrack(int var0, RSAbstractArchive var1, int var2, int var3, int var4, boolean var5)
 	{
-		client.setMusicPlayerStatus(1);
-		client.setMusicTrackArchive(var1);
-		client.setMusicTrackGroupId(var2);
-		client.setMusicTrackFileId(var3);
-		client.setMusicTrackVolume(var4);
-		client.setMusicTrackBoolean(var5);
-		client.setPcmSampleLength(var0);
+		for (RSMusicSong musicSong : client.getMusicSongs())
+		{
+			if (musicSong.getMusicTrackGroupId() == var2)
+			{
+				client.setMusicPlayerStatus(1);
+				musicSong.setMusicTrackArchive(var1);
+				musicSong.setMusicTrackGroupId(var2);
+				musicSong.setMusicTrackFileId(var3);
+				musicSong.setMusicTrackVolume(var4);
+				musicSong.setMusicTrackBoolean(var5);
+				//musicSong.setPcmSampleLength(var0);
+				break;
+			}
+		}
 	}
 
 	@Inject
@@ -2762,16 +2769,19 @@ public abstract class RSClientMixin implements RSClient
 	@Override
 	public void setMusicVolume(int volume)
 	{
-		if (volume > 0 && client.getPreferences().getMusicVolume() <= 0 && client.getCurrentTrackGroupId() != -1)
+		for (RSMusicSong musicSong : client.getMusicSongs())
 		{
-			client.playMusicTrack(1000, client.getMusicTracks(), client.getCurrentTrackGroupId(), 0, volume, false);
-		}
+			if (volume > 0 && client.getPreferences().getMusicVolume() <= 0 && musicSong.getMusicTrackGroupId() != -1)
+			{
+				client.playMusicTrack(1000, client.getMusicTracks(), musicSong.getMusicTrackGroupId(), 0, volume, false);
+			}
 
-		client.getPreferences().setMusicVolume(volume);
-		client.setMusicTrackVolume(volume);
-		if (client.getMidiPcmStream() != null)
-		{
-			client.getMidiPcmStream().setPcmStreamVolume(volume);
+			client.getPreferences().setMusicVolume(volume);
+			musicSong.setMusicTrackVolume(volume);
+			if (musicSong.getMidiPcmStream() != null)
+			{
+				musicSong.getMidiPcmStream().setPcmStreamVolume(volume);
+			}
 		}
 	}
 

runescape-client/src/main/java/ObjectComposition.java
@@ -973,15 +973,15 @@ public class ObjectComposition extends DualNode {
 				if (class306.field3398.isEmpty()) {
 					var6 = false;
 				} else {
-					class318 var7 = (class318)class306.field3398.get(0);
-					var6 = var7 != null && var5 == var7.field3507;
+					MusicSong var7 = (MusicSong)class306.field3398.get(0);
+					var6 = var7 != null && var5 == var7.musicTrackGroupId;
 				}
 
 				if (!var6 && class150.clientPreferences.method2478() != 0) {
 					ArrayList var9 = new ArrayList();
 
 					for (int var8 = 0; var8 < var0.size(); ++var8) {
-						var9.add(new class318(class13.archive6, (Integer)var0.get(var8), 0, class150.clientPreferences.method2478(), false));
+						var9.add(new MusicSong(class13.archive6, (Integer)var0.get(var8), 0, class150.clientPreferences.method2478(), false));
 					}
 
 					if (Client.playingJingle) {

runescape-client/src/main/java/class405.java
@@ -8,7 +8,7 @@ public class class405 extends class407 {
 	@ObfuscatedSignature(
 		descriptor = "Lmj;"
 	)
-	class318 field4520;
+	MusicSong field4520;
 	@ObfuscatedName("ay")
 	@ObfuscatedGetter(
 		intValue = 2025464221
@@ -25,9 +25,9 @@ public class class405 extends class407 {
 		super.field4528 = "FadeOutTask";
 		if (var2 >= 0) {
 			if (var3 && var2 < class306.field3396.size()) {
-				this.field4520 = (class318)class306.field3396.get(var2);
-			} else if (!var3 && var2 < class306.field3394.size()) {
-				this.field4520 = (class318)class306.field3394.get(var2);
+				this.field4520 = (MusicSong)class306.field3396.get(var2);
+			} else if (!var3 && var2 < class306.musicSongs.size()) {
+				this.field4520 = (MusicSong)class306.musicSongs.get(var2);
 			}
 
 			this.field4521 = var4;
@@ -40,19 +40,19 @@ public class class405 extends class407 {
 		garbageValue = "994462530"
 	)
 	public boolean vmethod7676(int var1) {
-		if (this.field4520 != null && this.field4520.field3516 != null) {
+		if (this.field4520 != null && this.field4520.midiPcmStream != null) {
 			this.field4520.field3515 = true;
 
 			try {
-				if (this.field4520.field3511 > 0.0F && this.field4520.field3516.isReady()) {
-					float var2 = this.field4521 == 0 ? (float)this.field4521 : (float)this.field4520.field3510 / (float)this.field4521;
-					class318 var10000 = this.field4520;
-					var10000.field3511 -= 0.0F == var2 ? (float)this.field4520.field3510 : var2;
+				if (this.field4520.field3511 > 0.0F && this.field4520.midiPcmStream.isReady()) {
+					float var2 = this.field4521 == 0 ? (float)this.field4521 : (float)this.field4520.musicTrackVolume / (float)this.field4521;
+					MusicSong var10000 = this.field4520;
+					var10000.field3511 -= 0.0F == var2 ? (float)this.field4520.musicTrackVolume : var2;
 					if (this.field4520.field3511 < 0.0F) {
 						this.field4520.field3511 = 0.0F;
 					}
 
-					this.field4520.field3516.setPcmStreamVolume((int)this.field4520.field3511);
+					this.field4520.midiPcmStream.setPcmStreamVolume((int)this.field4520.field3511);
 					return false;
 				}
 			} catch (Exception var4) {

runescape-client/src/main/java/class406.java
@@ -13,7 +13,7 @@ public class class406 extends class407 {
 	@ObfuscatedSignature(
 		descriptor = "Lmj;"
 	)
-	class318 field4524;
+	MusicSong field4524;
 	@ObfuscatedName("ay")
 	@ObfuscatedGetter(
 		intValue = 870968583
@@ -30,9 +30,9 @@ public class class406 extends class407 {
 		super.field4528 = "FadeInTask";
 		if (var2 >= 0) {
 			if (var3 && var2 < class306.field3396.size()) {
-				this.field4524 = (class318)class306.field3396.get(var2);
-			} else if (!var3 && var2 < class306.field3394.size()) {
-				this.field4524 = (class318)class306.field3394.get(var2);
+				this.field4524 = (MusicSong)class306.field3396.get(var2);
+			} else if (!var3 && var2 < class306.musicSongs.size()) {
+				this.field4524 = (MusicSong)class306.musicSongs.get(var2);
 			}
 
 			this.field4523 = var4;
@@ -45,19 +45,19 @@ public class class406 extends class407 {
 		garbageValue = "994462530"
 	)
 	public boolean vmethod7676(int var1) {
-		if (this.field4524 != null && this.field4524.field3516 != null) {
+		if (this.field4524 != null && this.field4524.midiPcmStream != null) {
 			this.field4524.field3515 = true;
 
 			try {
-				if (this.field4524.field3511 < (float)this.field4524.field3510 && this.field4524.field3516.isReady()) {
-					float var2 = this.field4523 == 0 ? (float)this.field4523 : (float)this.field4524.field3510 / (float)this.field4523;
-					class318 var10000 = this.field4524;
-					var10000.field3511 += var2 == 0.0F ? (float)this.field4524.field3510 : var2;
-					if (this.field4524.field3511 > (float)this.field4524.field3510) {
-						this.field4524.field3511 = (float)this.field4524.field3510;
+				if (this.field4524.field3511 < (float)this.field4524.musicTrackVolume && this.field4524.midiPcmStream.isReady()) {
+					float var2 = this.field4523 == 0 ? (float)this.field4523 : (float)this.field4524.musicTrackVolume / (float)this.field4523;
+					MusicSong var10000 = this.field4524;
+					var10000.field3511 += var2 == 0.0F ? (float)this.field4524.musicTrackVolume : var2;
+					if (this.field4524.field3511 > (float)this.field4524.musicTrackVolume) {
+						this.field4524.field3511 = (float)this.field4524.musicTrackVolume;
 					}
 
-					this.field4524.field3516.setPcmStreamVolume((int)this.field4524.field3511);
+					this.field4524.midiPcmStream.setPcmStreamVolume((int)this.field4524.field3511);
 					return false;
 				}
 			} catch (Exception var4) {